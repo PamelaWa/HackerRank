636ff264cb7a06f2070ab5f3e233cd1e689fe019
Pamela <49489753+PamelaWa@users.noreply.github.com>
2022-03-24 12:11:19 -0400

    Create Easy.sql

---
+++ Easy.sql
@@ -0,0 +1,13 @@
+--Revising the Select Query I
+SELECT *
+FROM CITY
+WHERE population > 100000 AND countrycode = 'USA'
+
+--Revising the Select Query II
+SELECT name
+FROM city
+WHERE population > 120000 AND countrycode = 'USA'
+
+
+
+
