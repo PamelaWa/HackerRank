1521c94356542f42687c8125032945938fdb92a6
Pamela <49489753+PamelaWa@users.noreply.github.com>
2022-03-25 11:43:07 -0400

    Update Easy.sql

--- Easy.sql
+++ Easy.sql
@@ -8,6 +8,24 @@ SELECT name
 FROM city
 WHERE population > 120000 AND countrycode = 'USA'
 
+--Select By ID
+SELECT *
+FROM city
+WHERE ID = 1661;
+
+--Japanese Cities' Attributes
+SELECT *
+FROM city
+WHERE countrycode = 'JPN';
+
+--
+
+
+
+
+
+
+
 
 
 
