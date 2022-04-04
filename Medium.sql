b9b91367a8b5c8eecaa5be02dfeca2680065be25
Pamela <49489753+PamelaWa@users.noreply.github.com>
2022-04-04 14:27:13 -0400

    Update Medium.sql

--- Medium.sql
+++ Medium.sql
@@ -1,3 +1,15 @@
 --Weather Observation Station 18
 SELECT ROUND(ABS(MIN(LAT_N)-MAX(LAT_N))+ABS(MIN(LONG_W)-MAX(LONG_W)),4)
 FROM station
+
+--*The PADS
+SELECT CONCAT(name,CONCAT('(',LEFT(occupation,1),')'))
+FROM occupations
+ORDER BY name ASC;
+
+SELECT CONCAT('There are a total of', ' ', COUNT(occupation),' ',LCASE(occupation),'s','.')
+FROM occupations
+GROUP BY occupation
+ORDER BY COUNT(occupation) ASC, occupation;
+
+
